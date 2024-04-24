f0260cd74d0d0f552478e1a8540fbb89c99ed365
pontakornth <pontakorn61@gmail.com>
2024-04-24 14:59:48 +0700

    Add navigation info

---
+++ app/src/main/java/dev/pontakorn/habitbudget/HabitBudgetNavigationInfo.kt
@@ -0,0 +1,41 @@
+package dev.pontakorn.habitbudget
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class HabitBudgetNavigationInfo(
+    val label: String = "",
+    val icon: ImageVector = Icons.Filled.Star,
+    val route: String = ""
+) {
+    fun getAllNavItems(): List<HabitBudgetNavigationInfo> {
+        return listOf(
+            HabitBudgetNavigationInfo(
+                label = "Transaction",
+                icon = Icons.Outlined.ThumbUp,
+                route = DestinationScreens.Transactions.route
+            ),
+            HabitBudgetNavigationInfo(
+                label = "Habit Tracking",
+                icon = Icons.Outlined.DateRange,
+                route = DestinationScreens.HabitTracking.route
+            ),
+            HabitBudgetNavigationInfo(
+                label = "Wallet",
+                icon = Icons.Outlined.AccountBox,
+                route = DestinationScreens.Wallets.route
+            ),
+            HabitBudgetNavigationInfo(
+                label = "Settings",
+                icon = Icons.Outlined.Settings,
+                route = DestinationScreens.Settings.route
+            )
+        )
+
+    }
+}
--- app/src/main/java/dev/pontakorn/habitbudget/MainActivity.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dev.pontakorn.habitbudget.ui.theme.HabitBudgetTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +43,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainScreenWithNavbar() {
+    val navController = rememberNavController()
+    var navSelectedItem by remember {
+        mutableIntStateOf(0)
+    }
+
+    Scaffold(bottomBar = {
+        NavigationBar {
+            HabitBudgetNavigationInfo().getAllNavItems().forEachIndexed { index, navInfo ->
+                NavigationBarItem(
+                    selected = index == navSelectedItem,
+                    onClick = {
+                        navSelectedItem = index
+                        navController.navigate(navInfo.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = { Icon(imageVector = navInfo.icon, contentDescription = navInfo.label) },
+                    label = {
+                        Text(text = navInfo.label)
+                    }
+                )
+            }
+        }
+    }) { paddingValues ->
+
+        NavHost(
+            navController = navController,
+            startDestination = DestinationScreens.Transactions.route,
+            modifier = Modifier.padding(paddingValues)
+
+        ) {
+            composable(route = DestinationScreens.Transactions.route) {
+                TransactionsScreen()
+            }
+            composable(route = DestinationScreens.HabitTracking.route) {
+                // TODO: Add habit tracking screen
+            }
+            composable(route = DestinationScreens.Wallets.route) {
+                // TODO: Add wallet screen
+            }
+            composable(route = DestinationScreens.Settings.route) {
+                // TODO: Add setting screen
+            }
+
+        }
+    }
+
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -39,8 +107,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     HabitBudgetTheme {
-        Greeting("Android")
+        MainScreenWithNavbar()
     }
 }
