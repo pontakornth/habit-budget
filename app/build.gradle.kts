2fc76ce275985d1d26f08b740a06a529ec848157
pontakornth <pontakorn61@gmail.com>
2024-04-28 15:04:21 +0700

    Add de-sugarging dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -69,6 +70,7 @@ dependencies {
     // Compose calendar
     implementation(libs.composecalendar)
     implementation(libs.composecalendar.kotlinx.datetime)
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
 
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation(libs.androidx.room.ktx)
