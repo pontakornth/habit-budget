bcfbe372e904330368e93d25453ecb158593d82f
pontakornth <pontakorn61@gmail.com>
2024-04-28 13:58:50 +0700

    Add dummy habit tracking screen

--- app/src/main/java/dev/pontakorn/habitbudget/ui/habit/HabitTrackingScreen.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/ui/habit/HabitTrackingScreen.kt
@@ -1,19 +1,24 @@
 package dev.pontakorn.habitbudget.ui.habit
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.pontakorn.habitbudget.DestinationScreens
 import dev.pontakorn.habitbudget.ui.theme.HabitBudgetTheme
+import io.github.boguszpawlowski.composecalendar.StaticCalendar
 
 
 @Composable
@@ -25,12 +30,28 @@ fun HabitTracking() {
                 .padding(vertical = 16.dp, horizontal = 32.dp),
             color = MaterialTheme.colorScheme.background
         ) {
-            Text(
-                text = "Habit Tracking",
-                fontWeight = FontWeight.Bold,
-                fontSize = 48.sp,
-                modifier = Modifier.fillMaxWidth()
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Habit Tracking",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 48.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                // TODO: Streak logic
+                StaticCalendar()
+                Text(
+                    modifier = Modifier.padding(vertical = 24.dp),
+                    text = "4 days streak",
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "I don't use money today.")
+                }
+            }
         }
     }
 }
