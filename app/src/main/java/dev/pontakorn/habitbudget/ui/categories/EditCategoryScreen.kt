053ca8be30b0215af7951e90c13b72a1dd68ef6b
pontakornth <pontakorn61@gmail.com>
2024-04-29 13:55:27 +0700

    Make edit category screen more generic
    It is intended to be used for both edit and add category

--- app/src/main/java/dev/pontakorn/habitbudget/ui/categories/EditCategoryScreen.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/ui/categories/EditCategoryScreen.kt
@@ -10,24 +10,46 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.pontakorn.habitbudget.data.CategoryType
 import dev.pontakorn.habitbudget.ui.theme.HabitBudgetTheme
 
 @Composable
-fun AddCategoryScreen() {
+fun EditCategoryScreen(
+    title: String = "Add Category",
+    categoryName: String = "",
+    onChangeCategoryName: (String) -> Unit = {},
+    currentType: CategoryType = CategoryType.EXPENSE,
+    onChangeCategoryType: (CategoryType) -> Unit = {},
+    currentIcon: ImageVector = Icons.Outlined.Star,
+    currentIconDescription: String = "placeholder",
+    onClickIconButton: () -> Unit = {},
+    onBackButtonClick: () -> Unit = {},
+    onConfirmButtonClick: () -> Unit = {}
+) {
+    val categoryTypes = listOf(CategoryType.EXPENSE, CategoryType.INCOME)
+    var typeDropdownOpen by remember {
+        mutableStateOf(false)
+    }
     HabitBudgetTheme {
         Surface(
             modifier = Modifier
@@ -40,7 +62,7 @@ fun AddCategoryScreen() {
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 Text(
-                    text = "Add Category",
+                    text = title,
                     fontWeight = FontWeight.Bold,
                     fontSize = 48.sp,
                     modifier = Modifier.fillMaxWidth()
@@ -61,8 +83,22 @@ fun AddCategoryScreen() {
                             fontWeight = FontWeight.Medium,
                             fontSize = 24.sp
                         )
-                        OutlinedButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Expense")
+                        Column {
+                            OutlinedButton(onClick = { typeDropdownOpen = true }) {
+                                Text(text = currentType.toString())
+                            }
+                            DropdownMenu(
+                                expanded = typeDropdownOpen,
+                                onDismissRequest = { typeDropdownOpen = false }) {
+                                categoryTypes.forEach { categoryType ->
+                                    DropdownMenuItem(text = {
+                                        categoryType.toString()
+                                    }, onClick = {
+                                        onChangeCategoryType(categoryType)
+                                        typeDropdownOpen = false
+                                    })
+                                }
+                            }
                         }
                     }
                     Row(
@@ -76,8 +112,8 @@ fun AddCategoryScreen() {
                             fontSize = 24.sp
                         )
                         OutlinedTextField(
-                            value = "",
-                            onValueChange = { TODO() },
+                            value = categoryName,
+                            onValueChange = onChangeCategoryName,
                             shape = RoundedCornerShape(size = 4.dp)
 
                         )
@@ -92,21 +128,26 @@ fun AddCategoryScreen() {
                             fontWeight = FontWeight.Medium,
                             fontSize = 24.sp
                         )
-                        OutlinedIconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Outlined.Star, contentDescription = "star icon")
+                        OutlinedIconButton(onClick = onClickIconButton) {
+                            Icon(
+                                imageVector = currentIcon,
+                                contentDescription = currentIconDescription
+                            )
                         }
 
 
                     }
                     Row(
-                        modifier = Modifier.fillMaxSize().padding(bottom = 32.dp),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = 32.dp),
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.Bottom
                     ) {
-                        Button(onClick = { /*TODO*/ }) {
+                        Button(onClick = onBackButtonClick) {
                             Text(text = "Back")
                         }
-                        Button(onClick = { /*TODO*/ }) {
+                        Button(onClick = onConfirmButtonClick) {
                             Text(text = "Confirm")
                         }
                     }
@@ -118,6 +159,6 @@ fun AddCategoryScreen() {
 
 @Preview(showBackground = true)
 @Composable
-fun AddCategoryScreenPreview() {
-    AddCategoryScreen()
+fun EditCategoryScreenPreview() {
+    EditCategoryScreen()
 }
