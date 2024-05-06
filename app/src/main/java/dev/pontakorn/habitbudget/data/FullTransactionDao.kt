31243b29c9024aefbe42cebfd9128cd8753397e3
pontakornth <pontakorn61@gmail.com>
2024-05-06 17:49:48 +0700

    Add transaction summary query

--- app/src/main/java/dev/pontakorn/habitbudget/data/FullTransactionDao.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/data/FullTransactionDao.kt
@@ -4,9 +4,9 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import androidx.room.Transaction as DatabaseTransaction
 import kotlinx.coroutines.flow.Flow
 import java.util.Date
+import androidx.room.Transaction as DatabaseTransaction
 
 interface FullTransactionDao {
     @DatabaseTransaction
@@ -46,6 +46,45 @@ interface FullTransactionDao {
         timeEnd: Date,
         transactionType: TransactionType
     ): Flow<List<FullTransaction>>
+
+    @DatabaseTransaction
+    @Query(
+        "SELECT SUM(IIF(transaction_type = :firstType, amount, 0)) as income, " +
+                " SUM(IIF(transaction_type = :secondType, amount, 0)) as expense, " +
+                "income - expense as balance from `transaction`"
+    )
+    fun getSummary(
+        firstType: TransactionType,
+        secondType: TransactionType
+    ): Flow<TransactionSummary>
+
+    fun getSummary(): Flow<TransactionSummary> =
+        getSummary(TransactionType.INCOME, TransactionType.EXPENSE)
+
+
+    @DatabaseTransaction
+    @Query(
+        "SELECT SUM(IIF(transaction_type = :firstType, amount, 0)) as income, " +
+                " SUM(IIF(transaction_type = :secondType, amount, 0)) as expense, " +
+                "income - expense as balance from `transaction`" +
+                "WHERE t.transaction_time >= :timeBegin AND t.transaction_time < :timeEnd"
+    )
+    fun getSummaryDuring(
+        firstType: TransactionType,
+        secondType: TransactionType,
+        timeBegin: Date,
+        timeEnd: Date
+    ): Flow<TransactionSummary>
+
+    fun getSummaryDuring(timeBegin: Date, timeEnd: Date): Flow<TransactionSummary> {
+        return getSummaryDuring(
+            TransactionType.INCOME,
+            TransactionType.EXPENSE,
+            timeBegin,
+            timeEnd
+        )
+    }
+
 
     @Insert
     suspend fun insertTransaction(transaction: Transaction)
---
+++ app/src/main/java/dev/pontakorn/habitbudget/data/TransactionSummary.kt
@@ -0,0 +1,10 @@
+package dev.pontakorn.habitbudget.data
+
+/**
+ * Summary of transaction. Unit of money is in the smallest unit (e.g. satang)
+ */
+data class TransactionSummary(
+    val income: Int,
+    val expense: Int,
+    val balance: Int
+)
