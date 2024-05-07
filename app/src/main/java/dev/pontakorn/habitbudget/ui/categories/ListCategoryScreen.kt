93df776d5b30054949cd2b7c30c7dcc8850f77fe
pontakornth <pontakorn61@gmail.com>
2024-05-07 15:17:34 +0700

    Add hiltViewModel for injection

--- app/src/main/java/dev/pontakorn/habitbudget/ui/categories/ListCategoryScreen.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/ui/categories/ListCategoryScreen.kt
@@ -3,9 +3,11 @@ package dev.pontakorn.habitbudget.ui.categories
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 
 @Composable
-fun ListCategoryScreen(viewModel: ListCategoryViewModel) {
+fun ListCategoryScreen(navController: NavController, viewModel: ListCategoryViewModel = hiltViewModel()) {
     val categories = viewModel.uiState.collectAsState()
     CategoriesScreen(
         categories = categories.value,
