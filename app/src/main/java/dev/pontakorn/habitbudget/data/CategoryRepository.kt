64f596dde0c2e49cef94725fb557c3325ab17493
pontakornth <pontakorn61@gmail.com>
2024-05-08 19:56:05 +0700

    Allow get by ID

--- app/src/main/java/dev/pontakorn/habitbudget/data/CategoryRepository.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/data/CategoryRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface CategoryRepository {
     fun getAll(): Flow<List<Category>>
     fun getByType(categoryType: CategoryType): Flow<List<Category>>
+    fun getById(id: Int): Flow<Category>
     suspend fun insertCategory(category: Category)
 
     suspend fun deleteCategory(category: Category)
