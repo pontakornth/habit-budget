438218a628830f287ae17f5f96f50e437059e947
pontakornth <pontakorn61@gmail.com>
2024-05-08 16:17:12 +0700

    Allow adding categories from screen

--- app/src/main/java/dev/pontakorn/habitbudget/ui/setting/SettingsScreen.kt
+++ app/src/main/java/dev/pontakorn/habitbudget/ui/setting/SettingsScreen.kt
@@ -1,21 +1,29 @@
 package dev.pontakorn.habitbudget.ui.setting
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import dev.pontakorn.habitbudget.DestinationScreens
 import dev.pontakorn.habitbudget.ui.theme.HabitBudgetTheme
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(
+    navController: NavController
+) {
     HabitBudgetTheme {
         Surface(
             modifier = Modifier
@@ -24,12 +32,32 @@ fun SettingsScreen() {
             color = MaterialTheme.colorScheme.background
         ) {
 
-            Text(
-                text = "Settings",
-                fontWeight = FontWeight.Bold,
-                fontSize = 48.sp,
-                modifier = Modifier.fillMaxWidth()
-            )
+            Column {
+                Text(
+                    text = "Settings",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 48.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 32.dp)
+                ) {
+                    TextButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            navController.navigate(DestinationScreens.Categories.route)
+                        }) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Categories",
+                            textAlign = TextAlign.Left,
+                            fontSize = 24.sp
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -37,5 +65,5 @@ fun SettingsScreen() {
 @Preview(showBackground = true)
 @Composable
 fun SettingsScreenPreview() {
-    SettingsScreen()
+    SettingsScreen(navController = rememberNavController())
 }
